570443036a5c33cd3aec15c7f476b779907d0a60
gastav3 <gastav3@hotmail.com>
2018-04-07 23:31:31 +0200

    database redoing

--- database.sql
+++ database.sql
@@ -70,38 +70,6 @@ LOCK TABLES `actors_of_movie` WRITE;
 UNLOCK TABLES;
 
 --
--- Temporary view structure for view `all_movies_view`
---
-
-DROP TABLE IF EXISTS `all_movies_view`;
-/*!50001 DROP VIEW IF EXISTS `all_movies_view`*/;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
-/*!50001 CREATE VIEW `all_movies_view` AS SELECT 
- 1 AS `idMovie`,
- 1 AS `movieType`,
- 1 AS `movieName`,
- 1 AS `releaseYear`,
- 1 AS `genre`*/;
-SET character_set_client = @saved_cs_client;
-
---
--- Temporary view structure for view `all_movies_with_genre_action_view`
---
-
-DROP TABLE IF EXISTS `all_movies_with_genre_action_view`;
-/*!50001 DROP VIEW IF EXISTS `all_movies_with_genre_action_view`*/;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
-/*!50001 CREATE VIEW `all_movies_with_genre_action_view` AS SELECT 
- 1 AS `idMovie`,
- 1 AS `movieType`,
- 1 AS `movieName`,
- 1 AS `releaseYear`,
- 1 AS `genre`*/;
-SET character_set_client = @saved_cs_client;
-
---
 -- Table structure for table `customer`
 --
 
@@ -135,7 +103,7 @@ DROP TABLE IF EXISTS `director`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `director` (
   `idDirector` int(11) NOT NULL AUTO_INCREMENT,
-  `firstName` varchar(45) DEFAULT NULL,
+  `customerFirstName` varchar(45) DEFAULT NULL,
   `lastName` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`idDirector`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
@@ -272,42 +240,6 @@ UNLOCK TABLES;
 --
 -- Dumping routines for database 'gustava'
 --
-
---
--- Final view structure for view `all_movies_view`
---
-
-/*!50001 DROP VIEW IF EXISTS `all_movies_view`*/;
-/*!50001 SET @saved_cs_client          = @@character_set_client */;
-/*!50001 SET @saved_cs_results         = @@character_set_results */;
-/*!50001 SET @saved_col_connection     = @@collation_connection */;
-/*!50001 SET character_set_client      = utf8 */;
-/*!50001 SET character_set_results     = utf8 */;
-/*!50001 SET collation_connection      = utf8_general_ci */;
-/*!50001 CREATE ALGORITHM=UNDEFINED */
-/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
-/*!50001 VIEW `all_movies_view` AS select `movie`.`idMovie` AS `idMovie`,`movie`.`movieType` AS `movieType`,`movie`.`movieName` AS `movieName`,`movie`.`releaseYear` AS `releaseYear`,`movie`.`genre` AS `genre` from `movie` */;
-/*!50001 SET character_set_client      = @saved_cs_client */;
-/*!50001 SET character_set_results     = @saved_cs_results */;
-/*!50001 SET collation_connection      = @saved_col_connection */;
-
---
--- Final view structure for view `all_movies_with_genre_action_view`
---
-
-/*!50001 DROP VIEW IF EXISTS `all_movies_with_genre_action_view`*/;
-/*!50001 SET @saved_cs_client          = @@character_set_client */;
-/*!50001 SET @saved_cs_results         = @@character_set_results */;
-/*!50001 SET @saved_col_connection     = @@collation_connection */;
-/*!50001 SET character_set_client      = utf8 */;
-/*!50001 SET character_set_results     = utf8 */;
-/*!50001 SET collation_connection      = utf8_general_ci */;
-/*!50001 CREATE ALGORITHM=UNDEFINED */
-/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
-/*!50001 VIEW `all_movies_with_genre_action_view` AS select `movie`.`idMovie` AS `idMovie`,`movie`.`movieType` AS `movieType`,`movie`.`movieName` AS `movieName`,`movie`.`releaseYear` AS `releaseYear`,`movie`.`genre` AS `genre` from `movie` where (`movie`.`genre` = 'Action') */;
-/*!50001 SET character_set_client      = @saved_cs_client */;
-/*!50001 SET character_set_results     = @saved_cs_results */;
-/*!50001 SET collation_connection      = @saved_col_connection */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -318,4 +250,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-04-07 17:04:09
+-- Dump completed on 2018-04-07 23:29:39
